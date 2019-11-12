9b475dc67dbfacf7b9044d49ee3385f751cd80fd
YoonSuHyeon <96-05-03@naver.com>
2019-11-13 00:08:00 +0900

    2019-11-12 StockPrice fail

---
+++ Programmers/src/StackandQueue/StockPrice.java
@@ -0,0 +1,46 @@
+package StackandQueue;
+
+import java.util.ArrayList;
+
+public class StockPrice {
+    public static int[] solution(int[] prices) {
+        int[] answer = {};
+        ArrayList<Integer> tempstack =new ArrayList<>();
+        ArrayList<Integer> temp =new ArrayList<>();
+        for(int i = prices.length ;i>0;i--){
+            if(temp.size() ==0){
+                temp.add(prices[i-1]);
+                tempstack.add(0);
+            }else{
+                int count =0;
+
+                for(int j = temp.size();j>0;j--){
+                    if(temp.get(j-1)>=prices[i-1]) count++;
+
+                }
+                temp.add(prices[i-1]);
+                tempstack.add(count);
+            }
+        }
+        /*for(int i =0;i<temp.size();i++){
+            System.out.println(tempstack.get(i));
+         }*/
+        for(int i=0;i<prices.length;i++){
+
+            prices[i]=tempstack.remove(tempstack.size()-1);
+        }
+
+
+
+
+
+        return prices;
+    }
+    public static void main(String args[]){
+        int i[] = {1, 2,15,4,3,4,1,2};
+        int b[]=StockPrice.solution(i);
+        for(int ij =0 ;ij<b.length;ij++){
+            System.out.println(b[ij]);
+        }
+    }
+}
