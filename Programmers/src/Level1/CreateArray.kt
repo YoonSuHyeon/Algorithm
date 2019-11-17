16adac5193610aeb49d65f4c875b1881ae334eeb
YoonSuHyeon <96-05-03@naver.com>
2019-11-18 01:22:26 +0900

    2019-11-18 CreateArray  Success

--- Programmers/src/Level1/CreateArray.kt
+++ Programmers/src/Level1/CreateArray.kt
@@ -1,2 +1,15 @@
 package Level1
 
+fun createArraysolution(n: Long): IntArray {
+
+  val r =  n.toString()
+           .reversed()
+           .split("") //리스트로 만들어준다
+           .filter { it !="" }
+           .map { it.toInt() }
+           .toIntArray()
+    return IntArray(1)
+}
+fun main(){
+    createArraysolution(12345).forEach { println(it) }
+}
