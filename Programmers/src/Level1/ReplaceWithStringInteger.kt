7bd4b31e901a8aa9b6f2ffa5ede77b203fad1a50
YoonSuHyeon <96-05-03@naver.com>
2019-11-20 20:25:50 +0900

    2019-11-20 RepalceWithStringInteger  Success

---
+++ Programmers/src/Level1/ReplaceWithStringInteger.kt
@@ -0,0 +1,10 @@
+package Level1
+
+fun replaceWithStringIntegersolution(s: String): Int {
+
+    return s.toInt()
+}
+fun main(){
+    println(replaceWithStringIntegersolution("1234"))
+    println(replaceWithStringIntegersolution("-1234"))
+}
