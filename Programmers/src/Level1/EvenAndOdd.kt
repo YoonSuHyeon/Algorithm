b030352601a606b582e34e9e6bfad26e6d901e88
Yoon Su Hyeon <96-05-03@naver.com>
2019-11-22 21:49:45 +0900

    2019-11-22 EvenAndOdd success

--- Programmers/src/Level1/EvenAndOdd.kt
+++ Programmers/src/Level1/EvenAndOdd.kt
@@ -1,6 +1,15 @@
 package Level1
+fun evenAndOddSolution(num: Int): String {
+    var answer = ""
 
+    when(num%2){
+        0 -> answer="Even"
+        1 -> answer="Odd"
+        -1-> answer="Odd"  //음수일때도 계산을 해줘야한다..
+    }
+    return answer
+}
 
 fun main(){
-
+    println(evenAndOddSolution(-1))
 }
