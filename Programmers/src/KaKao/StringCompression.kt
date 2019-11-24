ae87eff0928c2b5709eef57e946fdbf9505360e8
Yoon Su Hyeon <96-05-03@naver.com>
2019-11-25 00:31:03 +0900

    2019-11-25 StringCompression Success

--- Programmers/src/KaKao/StringCompression.kt
+++ Programmers/src/KaKao/StringCompression.kt
@@ -1,29 +1,60 @@
 package KaKao
 fun stringCompressionSolution(s: String): Int {
-    var answer = 0
+    var answer = s.length
     var temp =""
     var prestring :String=""
     var countnum =1
-
-    for(i in 1..s.length/2){
+    for(i in 1..s.length/2){//  길이/2 만 검사를 하면 된다.
         for(j in 0 until s.length step i){
             if(j==0){
-                prestring =s.substring(j until  j+i)
-            }else {
-                if(j+i>s.length)
+                prestring=s.substring(j until j+i)
+            }else{
+                if(j+i>s.length){//다음이 마지막인데 길이보다 클때
+                    temp = if(countnum==1){
+                        temp+prestring+s.substring(j)
+                    }else{
+                        temp+countnum.toString()+prestring+s.substring(j)
+                    }
+                }else if(j+i==s.length){//다음이 마지막일때
+                    temp = if(prestring==s.substring(j until j+i)){
+                        countnum++
+                        temp+countnum.toString()+prestring
+                    }else{
+                        if(countnum==1){
+                            temp+prestring+s.substring(j)
+                        }else{
+                            temp+countnum.toString()+prestring+s.substring(j)
+                        }
+                    }
+                }else{
+                    if(prestring==s.substring(j until j+i)){
+                        countnum++
+                    }else{
+                        if(countnum==1){
+                            temp += prestring
+                            prestring=s.substring(j until j+i)
+                            countnum=1
+                        }else{
+                            temp= temp+countnum.toString()+prestring
+                            prestring=s.substring(j until j+i)
+                            countnum=1
+                        }
+
+                    }
+                }
+
             }
-
-
         }
-        println(temp)
-        if(temp.length<answer)answer=temp.length
-        prestring=""
-        temp=""
+      if(answer>temp.length)answer=temp.length
         countnum=1
-
+        temp=""
+        prestring=""
     }
+
+
+
     return answer
 }
 fun main(){
-    stringCompressionSolution("aabbaccc")
+    stringCompressionSolution("bbbbaccc")
 }
