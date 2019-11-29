39d1707440e38fe82c7f7c87b8ffa59c7bf111bc
YoonSuHyeon <96-05-03@naver.com>
2019-11-29 16:44:32 +0900

    2019-11-29 Max Success

--- Programmers/src/Sort/Max.kt
+++ Programmers/src/Sort/Max.kt
@@ -1,9 +1,9 @@
 package Sort
 fun maxSolution(numbers: IntArray): String {
-    return numbers.map { it.toString() }
-            .sortedByDescending { it[0] }
+    var temp = numbers.sortedWith(Comparator({num1: Int, num2:Int -> "$num2$num1".compareTo("$num1$num2")}))
             .joinToString("")
-
+    if(temp[0]=='0') return "0"
+    return  temp
 }
 fun main(){
     var numbers = intArrayOf(3,30,34,5,9)
