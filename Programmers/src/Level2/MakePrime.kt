6be5e8fbe21f7932fe991b0443df31a47ca9c3a4
YoonSuHyeon <96-05-03@naver.com>
2020-01-02 21:44:13 +0900

    2020-01-02 MakePrime Fail(1)

---
+++ Programmers/src/Level2/MakePrime.kt
@@ -0,0 +1,37 @@
+package Level2
+
+fun makePrimeSolution(nums: IntArray): Int {
+    var answer = 0
+    val temp  = HashSet<Int>()
+    var sum=0
+    for(i in 0..nums.size-3){//3개를 찾고 size이기떄문에 -4
+        for(j in i+1..nums.size-2){
+            for(k in j+1..nums.size-1){
+                temp.add(nums[i]+nums[j]+nums[k])
+                //println("${nums[i]} ${nums[j]} ${nums[k]}   ")
+            }
+        }
+    }
+    if(temp.size==0){
+        return answer
+    }else{
+        var temp1 = temp.toIntArray()
+        var test =true //소수
+        for(i in temp1.indices){
+            for(j in 2 until temp1[i]){
+                if(temp1[i]%j==0){
+                    test=false
+                    break;
+                }
+            }
+            if(test) answer++
+            test=true
+        }
+    }
+    //println(answer)
+    return answer
+}
+
+fun main(){
+    makePrimeSolution(intArrayOf(1,2,3,4))
+}
