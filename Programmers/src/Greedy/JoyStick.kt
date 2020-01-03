e7d8133bb9baed20ac2680ff32f5f2ae7eb1a9ee
YoonSuHyeon <96-05-03@naver.com>
2020-01-03 20:47:10 +0900

    2020-01-03 JoyStick Fail(1)

---
+++ Programmers/src/Greedy/JoyStick.kt
@@ -0,0 +1,22 @@
+package Greedy
+fun joyStickSolution(name: String): Int {
+    var answer = 0
+    val sample =HashMap<Char,Int>()
+    var i='A'
+    var count= 0
+    //(0,1,2,3,4,5,6,7,8,9,10,11,12,13,12,11,10,9,8,7,6,5,4,3,2,1) //알파벳순 최소 움직이는 수
+    for(i in 'A'..'N'){
+        sample[i] = count++
+    }
+    count =12
+    for(i in 'O'..'Z'){
+        sample[i] = count--
+    }
+
+    name.forEach { answer += sample[it]!!}
+    answer+=name.length-1
+    return answer
+}
+fun main(){
+    joyStickSolution("JAZ")
+}
