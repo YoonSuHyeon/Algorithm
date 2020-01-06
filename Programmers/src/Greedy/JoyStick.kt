88b66b7e5c067f099b4cd1459628e45575da4502
YoonSuHyeon <96-05-03@naver.com>
2020-01-06 15:32:26 +0900

    2020-01-06 JoyStick Success(95/100)

--- Programmers/src/Greedy/JoyStick.kt
+++ Programmers/src/Greedy/JoyStick.kt
@@ -1,43 +1,60 @@
-/*package Greedy
+package Greedy
 fun joyStickSolution(name: String): Int {
- /*   var answer =0
-    var temp =name.map { 'A' }
-            .joinToString("")
+    var answerf = 0
 
-    var i =0
-
-    //앞으로 할 경우
-    while(temp != name){
-        if('A' == name[i]){
-            i++
-            continue
-        }else if(name[i] >'N'){
-            answer = answer +(13-(name[i]-'N'))
-            temp =
-        }else{
-
+    var temp = name.map { 'A' }.toMutableList()
+    var i = 0
+    while (true) { //앞으로
+        if (temp.joinToString("") == name) {
+            break
         }
-    }
-    //뒤로 할 경우
-
-    while(temp != name){
-        if('A' == name[i]){
-            i++
-            continue
-        }else if(name[i] >'N'){
-            answer = answer +(13-(name[i]-'N'))
-            temp =
-        }else{
-
+        when {
+            name[i] == 'A' -> {
+                answerf++
+            }
+            name[i] > 'N' -> {
+                answerf += (13 - (name[i] - 'N')) + 1
+                temp[i] = name[i]
+            }
+            else -> {
+                answerf += (name[i] - 'A') + 1
+                temp[i] = name[i]
+            }
         }
+        i++
     }
+    answerf--
 
-
+    temp = name.map { 'A' }.toMutableList()
+    var answer =0
+    var j = 0
+    while (true) {//뒤로
+        if (temp.joinToString("") == name) {
+            break
+        }
+        when {
+            name[j] == 'A' -> {
+                answer++
+            }
+            name[j] > 'N' -> {
+                answer += (13 - (name[j] - 'N')) + 1
+                temp[j] = name[j]
+            }
+            else -> {
+                answer += (name[j] - 'A') + 1
+                temp[j] = name[j]
+            }
+        }
+        if(j==0)j=j+name.length-1
+        else j--
+    }
+    answer--
+    if(answerf<answer) answer=answerf
     println(answer)
     return answer
 
 
 }
 fun main(){
-    joyStickSolution("JAN")
-}*/
+    joyStickSolution("AZAAAZ")
+}
