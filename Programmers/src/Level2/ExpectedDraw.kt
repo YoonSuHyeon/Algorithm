ee98a2d970c65132ff15e559d0df9f30b42d1251
YoonSuHyeon <96-05-03@naver.com>
2020-01-08 17:03:53 +0900

    2020-01-08 ExpectedDraw init

---
+++ Programmers/src/Level2/ExpectedDraw.kt
@@ -0,0 +1,12 @@
+package Level2
+fun expectedDrawSolution(n: Int, a: Int, b: Int): Int {
+    var answer = 0
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    println("Hello Kotlin")
+
+    return answer
+}
+fun main(){
+    expectedDrawSolution(8,4,7)
+}
