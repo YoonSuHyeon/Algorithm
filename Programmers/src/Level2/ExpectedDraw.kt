783b72371e427940d6280e71c1520e0a8d10902a
YoonSuHyeon <96-05-03@naver.com>
2020-01-08 17:37:27 +0900

    2020-01-08 ExpectedDraw Success

--- Programmers/src/Level2/ExpectedDraw.kt
+++ Programmers/src/Level2/ExpectedDraw.kt
@@ -1,11 +1,16 @@
 package Level2
 fun expectedDrawSolution(n: Int, a: Int, b: Int): Int {
-    var answer = 0
-
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    println("Hello Kotlin")
-
-    return answer
+    var answer = 1
+    var tempA= a
+    var tempB =b
+    while(true){
+        if(((tempA+1)/2) == ((tempB+1)/2) ) return answer
+        else{
+                tempA=(tempA+1)/2
+                tempB=(tempB+1)/2
+                answer++
+            }
+    }
 }
 fun main(){
     expectedDrawSolution(8,4,7)
