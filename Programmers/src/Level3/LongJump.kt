ee563e98257de673cdbbea092d94e801fcfe8869
YoonSuHyeon <96-05-03@naver.com>
2020-01-22 21:02:33 +0900

    2020-01-22 Palindrome Fail

--- Programmers/src/Level3/LongJump.kt
+++ Programmers/src/Level3/LongJump.kt
@@ -4,7 +4,7 @@ fun longJumpSolution(n: Int): Long {
     jump(n,0,1) //시작을 1칸
     jump(n,0,2) //시작을 2칸
     println(answer)
-    return answer
+    return answer%1234567
 }
 fun jump(n:Int,dis:Int,degree:Int){//가야하는거리,간거리,가는정도
     if(dis+degree==n){
---
+++ Programmers/src/Level3/Palindrome.kt
@@ -0,0 +1,16 @@
+package Level3
+fun palindromeSolution(s: String): Int {
+    var answer = 0
+    for (i in 3 until s.length){ //3문자로 부터 가능 하기 때문에 3부터시작
+        for(j in s.indices){
+            if(j+i>s.length-1)break;
+
+        }
+    }
+
+
+    return answer
+}
+fun main(){
+    palindromeSolution("abcdcba")
+}
