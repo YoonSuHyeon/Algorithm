9edc9a3de19a14892c42e3c94bc18a8070b8193c
YoonSuHyeon <96-05-03@naver.com>
2020-02-09 21:38:37 +0900

    2020-02-09 LockAndKey Fail(1)

---
+++ Programmers/src/KaKao/LockAndKey.kt
@@ -0,0 +1,15 @@
+package KaKao
+fun LockAndKeySolution(key: Array<IntArray>, lock: Array<IntArray>): Boolean {
+    var answer = true
+    var keylist= ArrayList<Array<IntArray>>()
+    keylist.add(key)
+
+
+    return answer
+}
+
+
+fun main(){
+    LockAndKeySolution(arrayOf(intArrayOf(0,0,0), intArrayOf(1,0,0), intArrayOf(0,1,1))
+    , arrayOf(intArrayOf(1,1,1), intArrayOf(1,1,0), intArrayOf(1,0,1)))
+}
