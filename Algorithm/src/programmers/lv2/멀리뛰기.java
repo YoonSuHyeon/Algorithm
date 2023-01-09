d0053abdeb3d9b38cba1701d73b7838842106d12
YoonSuHyeon <96-05-03@naver.com>
2023-01-09 21:12:26 +0900

    멀리뛰기

---
+++ Algorithm/src/programmers/lv2/멀리뛰기.java
@@ -0,0 +1,43 @@
+package programmers.lv2;
+
+/**
+ * 효진이는 멀리 뛰기를 연습하고 있습니다. 효진이는 한번에 1칸, 또는 2칸을 뛸 수 있습니다. 칸이 총 4개 있을 때, 효진이는
+ * (1칸, 1칸, 1칸, 1칸)
+ * (1칸, 2칸, 1칸)
+ * (1칸, 1칸, 2칸)
+ * (2칸, 1칸, 1칸)
+ * (2칸, 2칸)
+ * 의 5가지 방법으로 맨 끝 칸에 도달할 수 있습니다. 멀리뛰기에 사용될 칸의 수 n이 주어질 때, 효진이가 끝에 도달하는 방법이 몇 가지인지 알아내,
+ * 여기에 1234567를 나눈 나머지를 리턴하는 함수, solution을 완성하세요. 예를 들어 4가 입력된다면, 5를 return하면 됩니다.
+ */
+public class 멀리뛰기 {
+
+    /**
+     * an+2 = an + an+1
+     *
+     * @param n
+     * @return
+     */
+    public static long solution(int n) {
+
+        if (n <= 2)
+            return n % 3;
+
+        long an = 1;
+        long an1 = 2;
+        long an2 = an + an1;
+
+        for (int i = 3; i < n; i++) {
+            an = an1;
+            an1 = an2;
+            an2 = (an + an1) % 1234567;
+        }
+
+        return an2;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("result = " + solution(2));
+    }
+}
