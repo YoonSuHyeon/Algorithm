d2407ec9aec6592540a5fc3168b604181e1f9192
YoonSuHyeon <96-05-03@naver.com>
2023-01-19 23:53:38 +0900

    카펫

---
+++ Algorithm/src/programmers/lv2/카펫.java
@@ -0,0 +1,29 @@
+package programmers.lv2;
+
+/**
+ * Leo는 카펫을 사러 갔다가 아래 그림과 같이 중앙에는 노란색으로 칠해져 있고 테두리 1줄은 갈색으로 칠해져 있는 격자 모양 카펫을 봤습니다.
+ * <p>
+ * Leo는 집으로 돌아와서 아까 본 카펫의 노란색과 갈색으로 색칠된 격자의 개수는 기억했지만, 전체 카펫의 크기는 기억하지 못했습니다.
+ * <p>
+ * Leo가 본 카펫에서 갈색 격자의 수 brown, 노란색 격자의 수 yellow가 매개변수로 주어질 때 카펫의 가로, 세로 크기를 순서대로 배열에 담아 return 하도록 solution 함수를 작성해주세요.
+ *
+ * 2차방정식 근의 공식을 사용해서 풀이
+ */
+public class 카펫 {
+
+    public static int[] solution(int brown, int yellow) {
+        double v = Math.pow(brown + 4, 2) - 16 * (brown + yellow);
+        double v2 = v < 0 ? -v : v;
+
+        double temp2 = Math.sqrt(v2);
+
+        int x1 = (int) (brown + 4 + temp2) / 4;
+
+        int y = (brown + yellow) / x1;
+        return new int[]{x1, y};
+    }
+
+    public static void main(String[] args) {
+        카펫.solution(10, 2);
+    }
+}
