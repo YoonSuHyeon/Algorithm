eb30adbdd465ee915f16782bff92fdea07f8ddd5
YoonSuHyeon <96-05-03@naver.com>
2023-06-26 13:54:14 +0900

    미로탈출명령어.java

--- Algorithm/src/programmers/lv3/미로탈출명령어.java
+++ Algorithm/src/programmers/lv3/미로탈출명령어.java
@@ -6,10 +6,86 @@ import java.util.List;
 
 public class 미로탈출명령어 {
 
+    private static final String IMPOSSIBLE = "impossible";
+
+    // ----------- 세번쨰 시도 ------------------
+    // 사전순으로 dfs를한다.(멈춰야하는 경우
+    public String solution3(int n, int m, int x, int y, int r, int c, int k) {
+
+        // k거리를 이동해서 갈 수 없는 경우
+        int distance = Math.abs(r - x) + Math.abs(c - y);
+        if (distance > k)
+            return IMPOSSIBLE;
+
+        // 최소거리로 간후 남은 거리를 (lr,rl,ud,du)와 같은 방법으로 할수 없는경우
+        if ((k - distance) % 2 == 1)
+            return IMPOSSIBLE;
+
+        //d -> l -> r -> u 진행 시킬수 있는 값
+        int downCnt = Math.max(r - x, 0);
+        int leftCnt = Math.max(y - c, 0);
+        int rightCnt = Math.max(c - y, 0);
+        int upCnt = Math.max(x - r, 0);
+
+        // 원상 복귀하는 경우(lr,rl,ud,du)
+        int pairCnt = (k - distance) / 2;
+
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < k; i++) {
+            if (downCnt > 0 || pairCnt > 0 && x < n) {
+                //down
+                answer.append("d");
+
+                if (downCnt > 0)
+                    downCnt--;
+                else {
+                    pairCnt--;
+                    upCnt += 1;
+                }
+                x++;
+            } else if (leftCnt > 0 || pairCnt > 0 && y > 1) {
+                //left
+                answer.append("l");
+
+                if (leftCnt > 0)
+                    leftCnt--;
+                else {
+                    pairCnt--;
+                    rightCnt += 1;
+                }
+                y--;
+
+            } else if (rightCnt > 0 || pairCnt > 0 && y < m) {
+                //right
+                answer.append("r");
+
+                if (rightCnt > 0)
+                    rightCnt--;
+                else {
+                    pairCnt--;
+                    leftCnt += 1;
+                }
+                y++;
+
+            } else {
+                //up
+                answer.append("u");
+
+                if (upCnt > 0)
+                    upCnt--;
+                else {
+                    upCnt--;
+                    downCnt += 1;
+                }
+                x--;
+            }
+        }
+        return answer.toString();
+    }
+
+
 // ---------------------- 첫번째 시도 (k만큼 u,d,l,r을 사용해서 만들 수 있는 경우를 전부 생성후 실제로 탈출 시도롤 해본다)---------------------------------------------------------------
 
-
-    private static final String IMPOSSIBLE = "impossible";
 
     private String[][] maze;
 
@@ -99,6 +175,6 @@ public class 미로탈출명령어 {
 
     public static void main(String[] args) {
         미로탈출명령어 미로탈출명령어 = new 미로탈출명령어();
-        System.out.println("미로탈출명령어 = " + 미로탈출명령어.solution(3, 4, 2, 3, 3, 1, 5));
+        System.out.println("미로탈출명령어 = " + 미로탈출명령어.solution3(3, 3, 1, 2, 3, 3, 4));
     }
 }
